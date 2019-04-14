fd99046b04a4450e7a6a0b35b9eb2f214467bf5a
Pankaj Warke <pnkjwrk3@gmail.com>
2019-04-14 21:50:38 +0530

    final for heroku

---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require("express")
+var cors = require("cors")
+var bodyParser = require("body-parser")
+var app = express()
+var port = process.env.PORT || 5010
+
+app.use(bodyParser.json())
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+var Contracts = require('./routes/contracts')
+
+app.use('/contracts', Contracts)
+
+app.listen(port, () => {
+    console.log("Server is running on port: " + port)
+})
+
+if (process.env.NODE_ENV === 'production') {
+  // Exprees will serve up production assets
+  app.use(express.static('client/build'));
+
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
